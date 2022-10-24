a83b68e1955311d17723ffc63ca10be49d72145f
Siddharth <siddharth99c@gmail.com>
2022-10-24 11:28:14 +0530

    chore: cleans unused packages and plugins

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -53,7 +53,6 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: path.join(__dirname, 'index.html'),
 		}),
-		new webpack.HotModuleReplacementPlugin(),
 		new webpack.DefinePlugin({
 			// See: https://github.com/necolas/react-native-web/issues/349
 			__DEV__: JSON.stringify(true),
